a88b7b02e98d61f928fcb802cbfa9231a16318a9
marcelosvloja20-cyber <marcelosvloja20@gmail.com>
2026-05-26 23:27:09 -0300

    Refactor ReactDOM rendering syntax

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-import "./index.css";
+// =====================================
+// ROOT APPLICATION
+// =====================================
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(
+
+  document.getElementById("root")
+
+).render(
+
   <React.StrictMode>
+
     <App />
+
   </React.StrictMode>
+
 );
